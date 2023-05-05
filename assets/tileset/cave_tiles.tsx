ee9669df7c5b4adba71288f8c4785af0f06d5abf
jollux_ <gunkunlapat@gmail.com>
2023-05-05 09:55:32 +0700

    fix tilesets

--- assets/tileset/cave_tiles.tsx
+++ assets/tileset/cave_tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="cave_tiles" tilewidth="32" tileheight="32" tilecount="336" columns="16">
- <image source="../../../../../../../Downloads/cave.png" width="512" height="672"/>
+ <image source="cave.png" width="512" height="672"/>
  <tile id="17">
   <properties>
    <property name="free" type="bool" value="false"/>
